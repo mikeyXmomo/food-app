8cfc8a27031cfcd1521babe815a4fee88aa05820
unknown <ilhamdiky12@gmail.com>
2020-10-28 14:08:03 +0700

    Add jsconfig.json

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
-import Nav from '../components/Nav';
+import Nav from '@/components/Nav';
 import Head from 'next/head';
-import Hero from '../components/Hero';
-import Apps from '../components/Apps';
-import Info from '../components/Info';
-import { DataInfoOne, DataInfoThree, DataInfoTwo } from '../components/Data';
-import Banner from '../components/Banner';
-import Footer from '../components/Footer';
-import Sidebar from '../components/Sidebar';
+import Hero from '@/components/Hero';
+import Apps from '@/components/Apps';
+import Info from '@/components/Info';
+import { DataInfoOne, DataInfoThree, DataInfoTwo } from '@/components/Data';
+import Banner from '@/components/Banner';
+import Footer from '@/components/Footer';
+import Sidebar from '@/components/Sidebar';
 
 export default function IndexPage() {
 	const [ isOpen, setisOpen ] = useState(false);
